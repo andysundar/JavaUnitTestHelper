1c1d968b94537f30753811a78da9be4135c04517
andysundar <AnindyaBandopadhyay@gmail.com>
2017-07-08 17:22:36 +0530

    Added method annotation handler

--- POJOTester/src/main/java/org/pojotester/reflection/annotation/ReflectionMethodLevel.java
+++ POJOTester/src/main/java/org/pojotester/reflection/annotation/ReflectionMethodLevel.java
@@ -1,5 +1,25 @@
 package org.pojotester.reflection.annotation;
 
+import java.lang.reflect.Method;
+
+import org.pojotester.annotation.method.CreateObjectMethod;
+import org.pojotester.annotation.method.ReadMethod;
+import org.pojotester.annotation.method.WriteMethod;
+
 public abstract class ReflectionMethodLevel {
 	
+	public static boolean isCreateMethod(final Method method){
+		boolean thisIsCreateMethod = method.isAnnotationPresent(CreateObjectMethod.class);
+		return thisIsCreateMethod;
+	}
+	
+	public static boolean isWriteMethod(final Method method){
+		boolean thisIsCreateMethod = method.isAnnotationPresent(WriteMethod.class);
+		return thisIsCreateMethod;
+	}
+	
+	public static boolean isReadMethod(final Method method){
+		boolean thisIsCreateMethod = method.isAnnotationPresent(ReadMethod.class);
+		return thisIsCreateMethod;
+	}
 }
