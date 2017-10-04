bc8adfb31da425491ee0a92ba123d862d0cf0b58
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2017-10-04 19:28:53 +0530

    Added test for Enum

--- POJOTester/src/test/java/org/pojotester/test/AssertObjectCreatorTest.java
+++ POJOTester/src/test/java/org/pojotester/test/AssertObjectCreatorTest.java
@@ -42,4 +42,13 @@ public class AssertObjectCreatorTest {
 			assertEquals(assertObject.getExpectedValue(), assertObject.getReturnedValue());
 		}
 	}
+	
+	@Test
+	public void testGetAssertObjects_WhenEnumArgument() {
+		String []packagesToScan = {"org.pojotester.pack.scan.**.dto.Test04.class"};
+		List<AssertObject<?>> assertObjects = assertObjectCreator.getAssertObjects(packagesToScan);
+		for(AssertObject<?> assertObject : assertObjects) {
+			assertEquals(assertObject.getExpectedValue(), assertObject.getReturnedValue());
+		}
+	}
 }
